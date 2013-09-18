955359aed0b4b3afa6ebd067b643c95cbf30c27c
Devlon Duthie <duthied@gmail.com>
2013-09-18 09:41:36 -0700

    added callback to db methods for returning err object instead of handling the error in the db method, added check for empty result on single lookup method to return 404 if resource wasn't found

--- url-blacklist.js
+++ url-blacklist.js
@@ -14,37 +14,57 @@ app.configure(function () {
 
 // endpoints
 
-// TODO: add a post endpoint for adding url
 // TODO: add a delete endpoint for removing a url
 // TODO: handle different versions of the endpoint
 
+// get a specific host_and_port + url resource from the db
+// return 404 if not found
 app.get('/urlinfo/:version/:host_and_port/:url', function(req, res) {
   var version = req.params.version,
     host_and_port = req.params.host_and_port,
     url = req.params.url;
 
-  db.find_one(host_and_port, url, function(url) {
-    console.log('url: ' + JSON.stringify(url));
-    res.send(JSON.stringify(url));
-  });
+  db.find_one(host_and_port, url,
+    function (url) { // success
+      console.log('url: ' + JSON.stringify(url));
+      if (url.length > 0) {
+        res.send(JSON.stringify(url));
+      } else {
+        res.send(404);
+      }
+    },
+    function (err) { // failure
+      res.send(err);
+    });
 });
 
+// list all the url models from the db
 app.get('/urlinfo/:version', function(req, res) {
-  var urls = db.find_all(function (urls) {
-    console.log('urls: ' + JSON.stringify(urls));
-    res.send(JSON.stringify(urls));
-  });
+  var urls = db.find_all(
+    function (urls) { // success
+      console.log('urls: ' + JSON.stringify(urls));
+      res.send(JSON.stringify(urls));
+    },
+    function (err) {  // failure
+      res.send(err);
+    });
 });
 
+// add a new model to the database, return the newly created model
+// TODO: add param validation return malformed request when params not present, etc.
 app.post('/urlinfo/:version/:host_and_port/:url', function(req, res){
   var version = req.params.version,
     host_and_port = req.params.host_and_port,
     url = req.params.url;
 
-  db.create_url(host_and_port, url, function(new_url) {
-    console.log('created url: ' + JSON.stringify(new_url));
-    res.send(JSON.stringify(new_url));
-  });
+  db.create_url(host_and_port, url, 
+    function (new_url) { //success
+      console.log('created url: ' + JSON.stringify(new_url));
+      res.send(JSON.stringify(new_url));
+    },
+    function (err) { // failure
+      res.send(err);
+    });
 });
 
 http.createServer(app).listen(app.get('port'), function () {
